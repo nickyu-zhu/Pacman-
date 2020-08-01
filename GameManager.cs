b406860e0b4358543a64045a898f0f0c9e49b015
nickyu-zhu <57522177+nickyu-zhu@users.noreply.github.com>
2020-08-01 14:07:10 +0800

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,109 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Globalization;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public GameObject pacman;
+    public GameObject blinky;
+    public GameObject clyde;
+    public GameObject inky;
+    public GameObject pinky;
+    public GameObject startPanel;
+    public GameObject gamePanel;
+    public GameObject StartCountDownPrefab;
+    public GameObject gameoverPrefab;
+    public GameObject winPrefab;
+    public AudioClip startClip;
+    public Text time;
+    public int nowEat;
+
+    public List<int> usingIndex = new List<int>();
+    public List<int> rawIndex = new List<int> { 0, 1, 2, 3 };
+   
+
+    private void Awake()
+    {
+        _instance = this;
+        Screen.SetResolution(1024, 768, false);
+        int tempCount = rawIndex.Count;
+        for(int i = 0;i< tempCount; i++)
+        {
+            int tempIndex = Random.Range(0, rawIndex.Count);
+            usingIndex.Add(rawIndex[tempIndex]);
+            rawIndex.RemoveAt(tempIndex);
+        }
+    }
+
+   public void Start()
+    {
+        SetGameState(false);
+        nowEat = 0;
+        // AudioSource.PlayClipAtPoint(startClip, Vector3.zero);
+        
+    }
+   private  void Update()
+    {
+        if (nowEat == 4 && pacman.GetComponent<PacmanMove>().enabled != false)
+        {
+            gamePanel.SetActive(false);
+            Instantiate(winPrefab);
+            StopAllCoroutines();
+            SetGameState(false);
+        }
+        if(nowEat == 4)
+        {
+            if(Input.anyKey)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    
+    public void OnStartButton()
+    {
+        StartCoroutine(PlayStartCountDown());
+        
+        AudioSource.PlayClipAtPoint(startClip, Vector3.zero);
+        startPanel.SetActive(false);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator PlayStartCountDown()
+    {
+        GameObject go = Instantiate(StartCountDownPrefab);
+        yield return new WaitForSeconds(4f);
+        Destroy(go);
+        SetGameState(true);
+        gamePanel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        
+    }
+    private void SetGameState(bool state)
+    {
+        pacman.GetComponent<PacmanMove>().enabled = state;
+        blinky.GetComponent<GhostMove>().enabled = state;
+        clyde.GetComponent<GhostMove>().enabled = state;
+        inky.GetComponent<GhostMove>().enabled = state;
+        pinky.GetComponent<GhostMove>().enabled = state;
+    }
+}
